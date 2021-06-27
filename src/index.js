98d860e23edf1a67efdab42d26bb025d93eefda5
unknown <you@example.com>
2021-06-27 19:31:16 +0400

    start

---
+++ src/index.js
@@ -0,0 +1,31 @@
+import React from 'react';
+import reactDom from 'react-dom';
+
+const AppHeader = () => {
+  return <h1>My Todo List</h1>;
+}
+
+const SearchPanel = () => {
+  return <input placeholder="search"/>;
+}
+
+const TodoList = () => {
+  return (
+    <ul>
+      <li>Learn React</li>
+      <li>Build Awesome App</li>
+    </ul>
+  )
+}
+
+const App = () => {
+  return (
+    <div>
+      <AppHeader/>
+      <SearchPanel/>
+      <TodoList/>
+    </div>
+  )
+}
+
+reactDom.render(<App/>,document.getElementById('container'));
